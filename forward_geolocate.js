f95286cf0234b178efe0cbfaf35ba8a05b090345
Lee Lup Yuen <luppy@appkaki.com>
2019-04-10 12:12:56 +0800

    Push sensor data to external server

--- forward_geolocate.js
+++ forward_geolocate.js
@@ -1,10 +1,55 @@
+//  Server URL that will receive sensor data. See https://github.com/lupyuen/gcloud-wifi-geolocation.
+const PUSH_HOST   = 'YOUR_SERVER.appspot.com';
+const PUSH_PATH   = '/push?token=YOUR_TOKEN';
+const PUSH_SECURE = true;  //  True for https, false for http.
+
 //  This Cloud Code Trigger is executed whenever a CoAP message is received.
 //  If the message contains "ssid0" and "rssi0" values, we forward the message to
 //  Cloud Code Function "geolocate" to perform the geolocation.
 //  Cloud Code Triggers must complete within 2 seconds. So we forward the processing to 
 //  another cloud function, which can execute up to 20 seconds.
 
-/*
+function pushSensorData(values, callback) {
+  //  Push the sensor data to the Google Cloud AppEngine Server running gcloud-wifi-geolocation.
+  //  See https://github.com/lupyuen/gcloud-wifi-geolocation
+  
+  //  Compose body for push request. Body looks like
+  //  {device:"my_device", tmp:28.1, latitude:1.23, longitude:1.23, accuracy:1.23}
+  const body = {};
+  values.forEach(keyValue => {
+    //  Rename geolocation_accuracy to accuracy.
+    const key = (keyValue.key === 'geolocation_accuracy') ? 'accuracy' : keyValue.key;
+    const value = keyValue.value;
+    const geo = keyValue.geo;
+    if (!key) { return; }
+    body[key] = value;
+    
+    //  Save the geolocation.
+    if (!geo) { return; }
+    body.latitude = geo.lat;
+    body.longitude = geo.long;
+  });
+  console.log('push', body);
+  
+  //  Push the sensor data. 
+  httpRequest({
+    host:   PUSH_HOST,  //  e.g. YOUR_SERVER.appspot.com
+    path:   PUSH_PATH,  //  e.g.  /push?token=YOUR_TOKEN
+    secure: PUSH_SECURE,
+    method: 'POST',
+    headers: {
+      Accept:     '*/*',
+      Connection: 'close',
+      'Content-Type':'application/json'
+    }
+  }, body, function(err, result) {
+    if (err) { console.log('push error'); console.error(err); return callback(err); }
+    console.log('push result', result);
+    if (callback) { return callback(null, result); }
+  });
+}
+
+/* Cloud Code Trigger convention:
    params: is an object with the keys:
     - action: one of 'write' | 'read'
     - thingToken: the thing that triggered the trigger
@@ -12,7 +57,6 @@
     - key: the key
     - value: the data sent
     - datetime: (can be null)
-
    callback: is a function to be called when the trigger ends can contain a
        parameter string *error* if the trigger needs to report an error.
 */
@@ -21,18 +65,28 @@ function trigger(params, callback) {
   const values = params.values;
   if (!values) { return callback(); }
   
-  //  values contains
-  //  [{"key":"ssid0","value":"88:41:fc:bb:00:00"},{"key":"rssi0","value":-82},
-  //	{"key":"ssid1","value":"88:41:fc:d6:00:00"},{"key":"rssi1","value":-91},
-  //  	{"key":"ssid2","value":"18:d6:c7:3c:00:00"},{"key":"rssi2","value":-92}]
+  //  values contains geolocation parameters:
+  //  [{"key":"device","value":"my_device_id"},
+  //   {"key":"ssid0","value":"88:41:fc:bb:00:00"},{"key":"rssi0","value":-82},
+  //   {"key":"ssid1","value":"88:41:fc:d6:00:00"},{"key":"rssi1","value":-91},
+  //   {"key":"ssid2","value":"18:d6:c7:3c:00:00"},{"key":"rssi2","value":-92}]
+  //  or sensor data:
+  //  [{"key":"device","value":"my_device_id"},
+  //   {"key":"tmp","value":"28.1"}]
   console.log('forward_geolocate', values);
   
   //  Look for ssid0 and rssi0 keys.
-  const findSSID0 = values.reduce((found, x) => (x.key == 'ssid0' ? x.value : found), null);
-  const findRSSI0 = values.reduce((found, x) => (x.key == 'rssi0' ? x.value : found), null);
-  if (findSSID0 || findRSSI0) {
-    //  Forward to cloud function without waiting for it to complete.
+  const ssid0 = values.reduce((found, x) => (x.key == 'ssid0' ? x.value : found), null);
+  const rssi0 = values.reduce((found, x) => (x.key == 'rssi0' ? x.value : found), null);
+  
+  //  If this is a valid geolocation request, forward to geolocate cloud function without waiting for it to complete.
+  if (ssid0 && rssi0) {
+    console.log('forward to geolocate');
   	thethingsAPI.cloudFunction('geolocate', params, function(err, res) {});
-  }  
-  callback();  
+    return callback();  
+  }
+  
+  //  Otherwise push the sensor data to the external server without waiting for it to complete.
+  pushSensorData(values, null);
+  return callback();
 }
