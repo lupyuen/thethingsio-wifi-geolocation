6f5cd674f0f895bdd45026fb5175c3b4a9d8d87c
Lee Lup Yuen <luppy@appkaki.com>
2019-05-14 22:24:07 +0800

    Transform raw temperature to computed temperature

--- forward_geolocate.js
+++ forward_geolocate.js
@@ -1,4 +1,5 @@
-//  Server URL that will receive sensor data. See https://github.com/lupyuen/gcloud-wifi-geolocation.
+//  Server URL that will receive sensor data. 
+//  See https://github.com/lupyuen/gcloud-wifi-geolocation.
 const PUSH_HOST   = 'YOUR_SERVER.appspot.com';
 const PUSH_PATH   = '/push?token=YOUR_TOKEN';
 const PUSH_SECURE = true;  //  True for https, false for http.
@@ -10,8 +11,8 @@ const PUSH_SECURE = true;  //  True for https, false for http.
 //  another cloud function, which can execute up to 20 seconds.
 
 function pushSensorData(values, callback) {
-  //  Push the sensor data to the Google Cloud AppEngine Server running gcloud-wifi-geolocation.
-  //  See https://github.com/lupyuen/gcloud-wifi-geolocation
+  //  Push the sensor data to the Google Cloud AppEngine Server running g
+  //  cloud-wifi-geolocation. See https://github.com/lupyuen/gcloud-wifi-geolocation
   
   //  Compose body for push request. Body looks like
   //  {device:"my_device", tmp:28.1, latitude:1.23, longitude:1.23, accuracy:1.23}
@@ -43,7 +44,11 @@ function pushSensorData(values, callback) {
       'Content-Type':'application/json'
     }
   }, body, function(err, result) {
-    if (err) { console.log('push error'); console.error(err); return callback(err); }
+    if (err) { 
+      console.log('push error', err); 
+      if (callback) { return callback(err); }
+      return;
+    }
     console.log('push result', result);
     if (callback) { return callback(null, result); }
   });
@@ -64,6 +69,7 @@ function trigger(params, callback) {
   if (params.action !== 'write') { return callback(); }  //  Interested only in update actions, not read.
   const values = params.values;
   if (!values) { return callback(); }
+  const thingToken = params.thingToken;
   
   //  values contains geolocation parameters:
   //  [{"key":"device","value":"my_device_id"},
@@ -72,21 +78,38 @@ function trigger(params, callback) {
   //   {"key":"ssid2","value":"18:d6:c7:3c:00:00"},{"key":"rssi2","value":-92}]
   //  or sensor data:
   //  [{"key":"device","value":"my_device_id"},
-  //   {"key":"tmp","value":"28.1"}]
+  //   {"key":"tmp","value":28.1}]
+  //  For raw temperature:
+  //  [{"key":"device","value":"my_device_id"},
+  //   {"key":"t","value":1744}]
   console.log('forward_geolocate', values);
   
   //  Look for ssid0 and rssi0 keys.
   const ssid0 = values.reduce((found, x) => (x.key == 'ssid0' ? x.value : found), null);
   const rssi0 = values.reduce((found, x) => (x.key == 'rssi0' ? x.value : found), null);
   
-  //  If this is a valid geolocation request, forward to geolocate cloud function without waiting for it to complete.
+  //  If this is a valid geolocation request, forward to "geolocate" Cloud Code 
+  //  Function without waiting for it to complete.
   if (ssid0 && rssi0) {
     console.log('forward to geolocate');
   	thethingsAPI.cloudFunction('geolocate', params, function(err, res) {});
     return callback();  
   }
   
-  //  Otherwise push the sensor data to the external server without waiting for it to complete.
+  //  Look for t, the raw temperature, and tmp, the computed temperature.
+  //  If raw temperature is found but not computed temperature,
+  //  forward to "transform" Cloud Code Function to transform and update the values.
+  //  Don't wait for the Cloud Code Function to complete.
+  const t = values.reduce((found, x) => (x.key == 't' ? x.value : found), null);
+  const tmp = values.reduce((found, x) => (x.key == 'tmp' ? x.value : found), null);
+  if (t && !tmp) { 
+    console.log('forward to transform');
+  	thethingsAPI.cloudFunction('transform', params, function(err, res) {});
+    return callback();  
+  }
+  
+  //  Otherwise push the sensor data to the external server without 
+  //  waiting for it to complete.
   pushSensorData(values, null);
   return callback();
 }
